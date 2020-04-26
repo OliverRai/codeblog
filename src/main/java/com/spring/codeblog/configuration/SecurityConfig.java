a964758a66dbc2d923965a46ec7e3548084218a0
OliverRai <raiane.mateus.oliveira@gmail.com>
2020-04-26 18:51:47 -0300

    ajuste delete

--- src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
+++ src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/",
             "/posts",
             "/posts/{id}",
-            "/postDetails",
+            "/postDetails"
     };
 
     @Override
